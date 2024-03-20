7abb2aee096be256490dcc6905242a37d6c78674
venkatvatturi <146656897+venkatvatturi@users.noreply.github.com>
2024-03-20 09:35:11 +0530

    spi_master_project_sir

---
+++ Mini Projects/Venkat Vatturi/spi_master_major_project/Testbench_part/driver.v
@@ -0,0 +1,18 @@
+class driver;
+mailbox gen2driv;
+transaction trans;
+virtual intf vif;
+function new(mailbox gen2driv ,virtual intf vif);
+this.gen2driv=gen2driv;
+this.vif=vif;
+endfunction
+task get_msg;
+forever begin
+@(vif.driv_cb);
+gen2driv.get(trans);
+vif.d_in=trans.d_in;
+trans.display("driver");
+@(vif.driv_cb);
+end
+endtask
+endclass
